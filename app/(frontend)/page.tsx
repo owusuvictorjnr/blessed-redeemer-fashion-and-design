c25b94906ee34ccd5a0c422a1a40b775aad44700
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-17 22:59:53 +0000

    landing page

---
+++ app/(frontend)/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import LandingPage from "@/components/LandingPage";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-200">
+      <section>
+        <LandingPage />
+      </section>
+    </div>
+  );
+}
