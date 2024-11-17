85fb516e42509e84cf190b88822633f670b1d832
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-17 23:01:00 +0000

    sliding text

---
+++ components/NewsSlider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+
+const SlidingText = () => {
+  return (
+    <div className="slider-container text-[#3F51B5]">
+      <div className="slider-content">
+        <div className="slider-text">
+          Advancing Freedom, Securing Prosperity
+        </div>
+        <div className="slider-text">Committed to Economic Growth</div>
+        <div className="slider-text">Together for a Better Ghana</div>
+        <div className="slider-text">Empowering the Future Generation</div>
+        <div className="slider-text">Strengthening Education and Health</div>
+      </div>
+    </div>
+  );
+};
+
+export default SlidingText;
+
+
