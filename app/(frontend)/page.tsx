75abfd7bd817fd29625e36a38c1d5ff8c80386a9
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-18 20:29:05 +0000

    import banners and shirt components

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import KidsBanner from "@/components/Banners/kidsBanner";
+import WomenBanner from "@/components/Banners/womenBanner";
 import FeaturedProd from "@/components/FeaturedProd";
 import { HomeCategories } from "@/components/HomeCategories";
 import LandingPage from "@/components/LandingPage";
+import NewProducts from "@/components/NewProducts";
+import Shirts from "@/components/Shirts";
 
 export default function Home() {
   return (
@@ -18,6 +22,22 @@ export default function Home() {
 
       <section>
         <FeaturedProd />
+      </section>
+
+      <section>
+        <KidsBanner />
+      </section>
+
+      <section>
+        <NewProducts />
+      </section>
+
+      <section>
+        <WomenBanner />
+      </section>
+
+      <section>
+        <Shirts />
       </section>
     </div>
   );
