f4c6cc758d5fcc984b332fa1774800ea8d24d1c6
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-18 13:19:54 +0000

    men page

---
+++ components/Home-Categories/men.tsx
@@ -0,0 +1,26 @@
+import allData from "@/utils/data";
+import Image from "next/image";
+import React from "react";
+
+export default function MenCategory() {
+  return (
+    <div className="flex flex-col items-center">
+      {allData.homeCategory.men.map((item) => (
+        <div key={item.slug} className="flex flex-col items-center">
+          <div className="relative w-16 h-16">
+            {/* Adjust size as needed */}
+            <Image
+              src={item.img}
+              alt={item.name}
+              height={64}
+              width={64}
+              className="rounded-full object-cover w-full h-full"
+            />
+          </div>
+          <span className="mt-2 text-sm font-medium capitalize">{item.name}</span>{" "}
+          {/* Add item name below image */}
+        </div>
+      ))}
+    </div>
+  );
+}
