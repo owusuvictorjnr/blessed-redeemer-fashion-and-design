a9b01a41414fd1e97098a7ab212f0b92330f9e3b
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-18 17:16:54 +0000

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,11 @@
-'use client'
-
+"use client";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="min-h-56 bg-gray-400 w-full">
+      <div className="bg-black text-center text-xs text-gray-300/45">
+        <h1 className="">&copy; 2024 developed by vitech solutions</h1>
+      </div>
+    </div>
+  );
 }
