27b1475d32ae7df047728cd851c92301e7502ef1
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-18 13:20:24 +0000

    unisex page

---
+++ components/Home-Categories/unisex.tsx
@@ -0,0 +1,28 @@
+import allData from "@/utils/data";
+import Image from "next/image";
+import React from "react";
+
+export default function UnisexCategory() {
+  return (
+    <div className="flex flex-col items-center">
+      {allData.homeCategory.unisex.map((item) => (
+        <div key={item.slug} className="flex flex-col items-center">
+          <div className="relative w-16 h-16">
+            {/* Adjust size as needed */}
+            <Image
+              src={item.img}
+              alt={item.name}
+              height={64}
+              width={64}
+              className="rounded-full object-cover w-full h-full"
+            />
+          </div>
+          <span className="mt-2 text-sm font-medium capitalize">
+            {item.name}
+          </span>{" "}
+          {/* Add item name below image */}
+        </div>
+      ))}
+    </div>
+  );
+}
