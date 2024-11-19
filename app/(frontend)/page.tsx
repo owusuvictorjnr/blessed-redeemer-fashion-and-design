2c27ab79aa41ece418c17f6a4f7f4f872a4a8697
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-19 12:00:41 +0000

    comment all comp

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -2,9 +2,9 @@
 
 // import KidsBanner from "@/components/Banners/kidsBanner";
 // import WomenBanner from "@/components/Banners/womenBanner";
-import FeaturedProd from "@/components/FeaturedProd";
-import { HomeCategories } from "@/components/HomeCategories";
-import LandingPage from "@/components/LandingPage";
+// import FeaturedProd from "@/components/FeaturedProd";
+// import { HomeCategories } from "@/components/HomeCategories";
+// import LandingPage from "@/components/LandingPage";
 // import NewProducts from "@/components/NewProducts";
 // import Shirts from "@/components/Shirts";
 
@@ -12,16 +12,16 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-200">
       <section>
-        <LandingPage />
+        {/* <LandingPage /> */}
       </section>
 
       {/* Categories */}
       <section className="">
-        <HomeCategories />
+        {/* <HomeCategories /> */}
       </section>
 
       <section>
-        <FeaturedProd />
+      {/* /  <FeaturedProd /> */}
       </section>
 
       <section>
