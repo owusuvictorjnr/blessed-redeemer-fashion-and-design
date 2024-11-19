b88a62d9d03e7c16ed73ca070b60cd00501a2a3e
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-19 11:49:55 +0000

    comment navbar

--- app/(frontend)/layout.tsx
+++ app/(frontend)/layout.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import Footer from "@/components/Footer";
-import FirstTopbar from "@/components/Navbar/firstTopbar";
-import SecondTopbar from "@/components/Navbar/secondTopbar";
+// import FirstTopbar from "@/components/Navbar/firstTopbar";
+// import SecondTopbar from "@/components/Navbar/secondTopbar";
 
 export default function FrontendLayout({
   children,
@@ -11,8 +11,8 @@ export default function FrontendLayout({
 }>) {
   return (
     <div className="">
-      <FirstTopbar />
-      <SecondTopbar />
+      {/* <FirstTopbar />
+      <SecondTopbar /> */}
       <main className="">{children}</main>
       <Footer />
     </div>
