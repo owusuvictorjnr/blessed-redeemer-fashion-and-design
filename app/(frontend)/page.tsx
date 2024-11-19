fd68a3bfc3921e61f1ad17d33f7706ea8d52ce25
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-19 12:15:58 +0000

    uncomment all components

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,30 +1,30 @@
 "use client";
 
-// import KidsBanner from "@/components/Banners/kidsBanner";
-// import WomenBanner from "@/components/Banners/womenBanner";
-// import FeaturedProd from "@/components/FeaturedProd";
-// import { HomeCategories } from "@/components/HomeCategories";
-// import LandingPage from "@/components/LandingPage";
-// import NewProducts from "@/components/NewProducts";
-// import Shirts from "@/components/Shirts";
+import KidsBanner from "@/components/Banners/kidsBanner";
+import WomenBanner from "@/components/Banners/womenBanner";
+import FeaturedProd from "@/components/FeaturedProd";
+import { HomeCategories } from "@/components/HomeCategories";
+import LandingPage from "@/components/LandingPage";
+import NewProducts from "@/components/NewProducts";
+import Shirts from "@/components/Shirts";
 
 export default function Home() {
   return (
     <div className="">
-      <section>{/* <LandingPage /> */}</section>
+      <section><LandingPage /></section>
 
       {/* Categories */}
-      <section className="">{/* <HomeCategories /> */}</section>
+      <section className=""><HomeCategories /></section>
 
-      <section>{/* /  <FeaturedProd /> */}</section>
+      <section>  <FeaturedProd /></section>
 
-      <section>{/* <KidsBanner /> */}</section>
+      <section><KidsBanner /></section>
 
-      <section>{/* <NewProducts /> */}</section>
+      <section><NewProducts /></section>
 
-      <section>{/* <WomenBanner /> */}</section>
+      <section><WomenBanner /></section>
 
-      <section>{/* <Shirts /> */}</section>
+      <section><Shirts /></section>
     </div>
   );
 }
