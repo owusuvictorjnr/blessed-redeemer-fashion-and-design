ec4eebef7c56f9185fda8c041bd52c520bd93225
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-20 00:14:38 +0000

    get currency props

---
+++ utils/getCurrencyProps.ts
@@ -0,0 +1,27 @@
+import { getCurrencyRate } from "./getCurrencyRate";
+import ipinfo from "node-ipinfo";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function getCurrencyProps(req: any) {
+  const token = process.env.NEXT_PUBLIC_IPINFO_TOKEN ?? "";
+  const ip = req.headers["x-real-ip"] || req.connection.remoteAddress;
+
+  let country = "Ghana"; // default country
+  try {
+    const ipInfo = new ipinfo(token);
+    const { country: userCountry } = await ipInfo.lookupIp(ip);
+    country = userCountry || "Ghana";
+  } catch (error) {
+    console.log("Error fetching user location: ", error);
+  }
+
+  const currency = country === "US" ? "USD" : "GHS";
+  const rate = await getCurrencyRate(currency);
+  const currencySymbol = country === "US" ? "$" : "₵";
+
+  return {
+    currency,
+    rate,
+    currencySymbol,
+  };
+}
