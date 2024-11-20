21f20505f8c720e7e27a8324f4b759422dddaad3
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-20 00:11:14 +0000

    shirt component

--- components/HomeCategories.tsx
+++ components/HomeCategories.tsx
@@ -4,48 +4,44 @@ import MenCategory from "./Home-Categories/men";
 import KidsCategory from "./Home-Categories/kids";
 import UnisexCategory from "./Home-Categories/unisex";
 import Link from "next/link";
+import ShirtsCategory from "./Home-Categories/shirts";
 
 export const HomeCategories = () => {
   return (
     <div className="px-4 py-6 mt-10 md:mt-20">
-      <div className="flex justify-between">
-        <h2 className="text-sm font-semibold mb-4">Shop By Category</h2>
-        <h4 className="text-sm text-gray-400 capitalize font-semibold mb-4 hover:text-blue-400">
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-sm font-semibold">Shop By Category</h2>
+        <h4 className="text-sm text-gray-400 capitalize font-semibold hover:text-blue-400">
           <Link href="">see all</Link>
         </h4>
       </div>
-      <div className="flex md:justify-center md:container md:mx-auto lg:gap-28  gap-6 overflow-x-auto">
-        <div className="flex flex-col items-center">
-          <Link href="/category/women">
-            <WomenCategory />
-          </Link>
-        </div>
 
-        <div className="flex flex-col items-center">
-          <Link href="/category/men">
-            <MenCategory />
-          </Link>
-        </div>
+      {/* Flex layout for small screens, grid layout for medium and larger screens */}
+      <div className="flex md:grid md:grid-cols-5 md:gap-6 gap-4 overflow-x-auto md:overflow-hidden">
+        <Link href="/category/women" className="flex flex-col items-center">
+          <WomenCategory />
+        </Link>
+        <Link href="/category/men" className="flex flex-col items-center">
+          <MenCategory />
+        </Link>
+        <Link href="/category/kids" className="flex flex-col items-center">
+          <KidsCategory />
+        </Link>
+        <Link href="/category/unisex" className="flex flex-col items-center">
+          <UnisexCategory />
+        </Link>
 
-        <div className="flex flex-col items-center">
-          <Link href="/category/kids">
-            <KidsCategory />
-          </Link>
-        </div>
+        <Link href="/category/unisex" className="flex flex-col items-center">
+          <ShirtsCategory />
+        </Link>
 
-        <div className="flex flex-col items-center">
-          <Link href="/category/unisex">
-            <UnisexCategory />
-          </Link>
-        </div>
       </div>
     </div>
   );
 };
 
+// TODO: on large screen, the categories should be displayed in a grid of 4 columns
 
-// new prod.
-// featured prod.
 // Tops
 // trousers
 // top & down
