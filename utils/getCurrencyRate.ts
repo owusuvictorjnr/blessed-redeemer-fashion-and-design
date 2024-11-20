c9d0def3c5f1247cea7b4ff3de3c067ba63cd08c
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-20 10:08:13 +0000

    add to .env

--- utils/getCurrencyRate.ts
+++ utils/getCurrencyRate.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getCurrencyRate = async (currencyCode: string) => {
   try {
     const response = await axios.get(
-      `https://api.exchangerate-api.com/v4/latest.json?app_id=${process.env.NEXT_PUBLIC_OPENEXCHANGE_APP_ID}base=USD`
+      `${process.env.EXT_PUBLIC_OPENEXCHANGE_BASE_URL}?app_id=${process.env.OPENEXCHANGE_APP_ID}`
     );
     return response.data.rates[currencyCode] || 1;
   } catch (error) {
