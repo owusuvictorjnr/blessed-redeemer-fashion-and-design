a1ddc3d04e24cae28937f488bb1e313b23132ff8
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-22 18:30:40 +0000

    map to homeShirt

--- components/Shirts.tsx
+++ components/Shirts.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { CiHeart } from "react-icons/ci";
-import { useCurrency } from "@/utils/CurrencyContext";
 
 // Define types for the product data
 interface Product {
@@ -18,14 +17,7 @@ interface Product {
 }
 
 export default function ShirtsComponent() {
-  const { rate, currencySymbol } = useCurrency(); // Get rate and symbol from context
-  const shirts: Product[] = allData.shirts.slice(0, 6);
-
-  // Utility function for price conversion
-  const convertPrice = (price: number) => {
-    if (!rate) return price.toFixed(2); // Fallback if rate isn't available yet
-    return (price * rate).toFixed(2);
-  };
+  const homeShirt: Product[] = allData.homeShirt.slice(0, 6);
 
   return (
     <div className="pb-5 min-h-[20rem]">
@@ -34,7 +26,7 @@ export default function ShirtsComponent() {
         <h2 className="text-sm font-semibold mb-4 capitalize lg:text-lg">
           shirts
         </h2>
-        <Link href="/shirts">
+        <Link href={`/category/$`}>
           <h4 className="text-sm text-gray-400 capitalize font-semibold mb-4 hover:text-blue-400 md:hidden">
             see all
           </h4>
@@ -43,7 +35,7 @@ export default function ShirtsComponent() {
 
       {/* Shirts Grid */}
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6 px-5 py-6">
-        {shirts.map(({ name, image, slug, rating, price, description }) => (
+        {homeShirt.map(({ name, image, slug, rating, price, description }) => (
           <div
             key={name}
             className="shadow-md bg-[#FFFAF0] min-h-[15rem] rounded"
@@ -69,7 +61,7 @@ export default function ShirtsComponent() {
               <div className="flex justify-center gap-5 items-center">
                 <p className="text-lg text-[#FFA500]">{rating}</p>
                 <p className="text-lg font-bold text-[#FF6B6B]">
-                  {currencySymbol} {convertPrice(price)}
+                  ${(price as number).toFixed(2)}
                 </p>
               </div>
               <p className="font-semibold text-[#9E9E9E]">
